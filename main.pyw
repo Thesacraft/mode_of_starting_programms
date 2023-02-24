445dcf3251bf75abd111a8965cf9d0b332850b86
Thesacraft <91694323+Thesacraft@users.noreply.github.com>
2023-02-24 17:36:18 +0100

    GUI

---
+++ main.pyw
@@ -0,0 +1,12 @@
+"""
+description: this is a basic programm that lets you start multiple programms with different mode's
+Author: Samuel B.
+Version: 1.0
+"""
+import start_programms
+
+app = start_programms.Open_programms_tkinter()
+app.run()
+
+
+
--- start_programms.py
+++ start_programms.py
@@ -7,7 +7,10 @@ Version: 1.0
 import os
 import platform
 import subprocess
+import sys
+import tkinter
 import webbrowser
+from tkinter.messagebox import showinfo
 
 import json
 
@@ -50,3 +53,26 @@ class Open_programms():
                     os.startfile(json_object[item])
                 else:
                     subprocess.Popen(json_object[item])
+
+class Open_programms_tkinter():
+    def __init__(self):
+        self.root = tkinter.Tk()
+        self.logic_class = Open_programms()
+
+    def _items_selected(self, event):
+        # get all selected indices
+        selected_mode = self.listbox.get(self.listbox.curselection()[0])
+        # get selected items
+        msg = f'Sarting programms of mode: {selected_mode}'
+        self.logic_class.setup(selected_mode)
+        self.logic_class.startProgramms()
+        self.root.quit()
+
+    def _setup(self):
+        var = tkinter.Variable(value=self.logic_class.modes)
+        self.listbox = tkinter.Listbox(self.root, listvariable=var, height=15,width=50, selectmode=tkinter.SINGLE,justify="center")
+        self.listbox.pack()
+        self.listbox.bind('<Double-1>', self._items_selected)
+    def run(self):
+        self._setup()
+        self.root.mainloop()
